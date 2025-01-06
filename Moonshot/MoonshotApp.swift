95906b2595b3e3395d7664e4ca5a087cbd2362c5
xHadi3 <hadiessam9@gmail.com>
2025-01-06 15:52:39 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Hadi Al zayer on 06/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
