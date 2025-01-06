403f86e7165b86524886f7cff976929e0eaf8b14
xHadi3 <hadiessam9@gmail.com>
2025-01-06 23:42:52 +0300

    adding some files

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,18 @@
+//
+//  astronaut.swift
+//  Moonshot
+//
+//  Created by Hadi Al zayer on 06/07/1446 AH.
+//
+
+import SwiftUI
+
+struct astronaut: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    astronaut()
+}
