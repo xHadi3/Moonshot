3d0e11652745b5e8a27575e585ff3e7c9f09bbcb
xHadi3 <hadiessam9@gmail.com>
2025-01-16 00:00:23 +0300

    adding the missions detail page

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Hadi Al zayer on 15/07/1446 AH.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
