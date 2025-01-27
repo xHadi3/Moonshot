a7f1817d52bf77e9570b3fe5dd16ad70eaef4048
xHadi3 <hadiessam9@gmail.com>
2025-01-27 18:06:16 +0300

    adding the detail pages

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Hadi Al zayer on 27/07/1446 AH.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautView()
+}
