9d8e39e4adb4ab90e88fb4fc4a3bff5214b751e5
xHadi3 <hadiessam9@gmail.com>
2025-01-28 15:27:39 +0300

    finishing the challenge

---
+++ Moonshot/GridLayout.swift
@@ -0,0 +1,18 @@
+//
+//  GridLayout.swift
+//  Moonshot
+//
+//  Created by Hadi Al zayer on 28/07/1446 AH.
+//
+
+import SwiftUI
+
+struct GridLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridLayout()
+}
---
+++ Moonshot/ListLayout.swift
@@ -0,0 +1,18 @@
+//
+//  ListLayout.swift
+//  Moonshot
+//
+//  Created by Hadi Al zayer on 28/07/1446 AH.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListLayout()
+}
